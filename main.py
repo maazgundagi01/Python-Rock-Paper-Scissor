398cc327d956787a876f1418da1f464b5a708bc5
Maz Gunn <maazgundagi@gmail.com>
2025-08-20 15:14:16 -0500

    initialized main.py with choice and comp_choice declarations and random int generation functionality

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+emotes = [
+
+]
+choice = input('Welcome, to Rock Paper Scissor Game!\nPlease type in "r" for rock, "p" for paper, "s" for scissors')
+comp_choice = random.randint(1,3)
