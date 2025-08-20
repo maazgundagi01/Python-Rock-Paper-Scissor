ccf0aebb60caf6e6aefb86ec112f29e4d06c2a7b
Maz Gunn <maazgundagi@gmail.com>
2025-08-20 18:42:30 -0500

    updated actions.py

--- actions.py
+++ actions.py
@@ -1,29 +1,31 @@
-rock = '''
+def print_action(label, action):
+    if action == 'rock':
+        print(label)
+        print('''
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-'''
-
-paper = '''
+        ''')
+    elif action == 'paper':
+        print(label)
+        print('''
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-'''
-
-scissors = '''
-    _______
+        ''')
+    elif action == 'scissors':
+        print(label)
+        print('''
+    ______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-'''
-
-def print_action(action):
-    print(action)
+        ''')
