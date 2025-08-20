8b521b2c68625c123ec216136eb7001a9eb238dd
Maz Gunn <maazgundagi@gmail.com>
2025-08-20 17:43:52 -0500

    finished the core game logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
-emotes = [
-
-]
-choice = input('Welcome, to Rock Paper Scissor Game!\nPlease type in "r" for rock, "p" for paper, "s" for scissors')
-comp_choice = random.randint(1,3)
+import actions
+words = ["rock", "paper", "scissors"]
+emotes = [actions.rock, actions.paper, actions.scissors]
+p_choice = input('Welcome, to Rock Paper Scissor Game!\nPlease type in "rock", "paper" or "scissors"').lower()
+actions.print_action(p_choice)
+c_random = random.randint(0,2)
+c_choice = words[c_random]
