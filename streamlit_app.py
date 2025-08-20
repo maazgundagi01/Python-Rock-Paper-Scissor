651aff8a12c0ad55b9c183dd2c0e660d3554ef9c
Maz Gunn <maazgundagi@gmail.com>
2025-08-20 18:15:57 -0500

    streamlit minor title adjustments

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def get_hand_emoji(choice):
 
 
 # Main app
-st.title("Rock Paper Scissors Game! 🪨📄✂️")
+st.title("🪨📄✂️\nRock Paper Scissors Game!")
 st.write("Welcome, to Rock Paper Scissor Game!")
 
 # Game logic
